7c5ab7beb3417760116c1d187e6dfbab287ec127
Arvind Naidu <arnaidu@paypal.com>
2015-03-24 19:15:17 -0700

    Added Pisces support

--- client/src/jsx/tools.pisces.jsx
+++ client/src/jsx/tools.pisces.jsx
@@ -1,11 +1,12 @@
-var ContentMixin = require('../mixins/content');
+var React = require('react'),
+    ContentMixin = require('../mixins/content');
 
 ContentMixin.i18n = function (key) {
     if(typeof key === "string") {
-      // key = this.props.dictionary[key];  
-      return "REDACTED";  
+        key = this.props.dictionary[key];
     }
-    return "REDACTED";
+    return (<edit data-key={key.contentKey} data-bundle={key.contentBundle} data-original={key.value}>{key.value}</edit>)
+
 };
 
 module.exports = {
--- client/src/mixins/content.js
+++ client/src/mixins/content.js
@@ -1,13 +1,4 @@
 var ContentMixin = {
-  componentWillMount: function() {
-    
-  },
-  setInterval: function() {
-    
-  },
-  componentWillUnmount: function() {
-    
-  },
   i18n : function (key) {
       if(typeof key === "string") {
         key = this.props.dictionary[key];  
