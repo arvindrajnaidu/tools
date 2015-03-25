6c895f79d8780999a577e294cd797433c0d752e4
Arvind Naidu <arnaidu@paypal.com>
2015-03-24 19:18:17 -0700

    Added sinon

--- client/test/test.js
+++ client/test/test.js
@@ -3,14 +3,14 @@
 require.config( {
     paths: {
         'jquery' : 'https://code.jquery.com/jquery-2.1.3.min',
+        'sinon': 'https://cdnjs.cloudflare.com/ajax/libs/sinon.js/1.7.3/sinon-min',
         'tools': '../dist/js/tools.pisces',
-        'text': 'text'
-        // ,
-        // 'pisces': 'http://pisces-168854.slc01.dev.ebayc3.com/js/pisces',
+        'text': 'text',
+        'pisces': 'http://pisces-168854.slc01.dev.ebayc3.com/js/pisces',
     }
 });
 
-require(['jquery', 'tools', 'text!fixtures/dropdown.json', 'text!fixtures/tools.json'], function (jQuery, Tools, dropdownMock, toolsDataMock) {
+require(['jquery', 'sinon', 'tools', 'text!fixtures/dropdown.json', 'text!fixtures/tools.json', 'pisces'], function (sinon, jQuery, Tools, dropdownMock, toolsDataMock) {
 
     sinon.stub(jQuery, "ajax", function(options) {
         if (options.url.indexOf("/content?bundle=dropdown") > -1) {
